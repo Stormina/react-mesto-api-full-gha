1d5252c571c4a30ce7650ffe04d2914e36518702
Максим Сычев <Dethstorm@mail.ru>
2023-04-23 19:50:23 +0300

    добавить CORS

--- backend/app.js
+++ backend/app.js
@@ -29,12 +29,12 @@ const allowedCors = [
 app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-  /* res.header('Access-Control-Allow-Origin', origin); */
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers['access-control-request-headers, origin, content-type'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
