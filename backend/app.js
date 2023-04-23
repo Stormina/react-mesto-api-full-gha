38978c5a26d88792b20151f53b274ca2f896bd7b
Максим Сычев <Dethstorm@mail.ru>
2023-04-23 18:52:43 +0300

    добавить CORS

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
 app.use('/', router);
