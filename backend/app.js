903584e83ea24762a7e1347f1d3ba2230d198eef
Максим Сычев <Dethstorm@mail.ru>
2023-04-23 19:12:34 +0300

    добавить CORS

--- backend/app.js
+++ backend/app.js
@@ -22,14 +22,15 @@ const allowedCors = [
   'localhost:3000',
   'https://localhost:3000',
   'https://127.0.0.1:3000',
-  'https://stormina.students.nomoredomains.monster',
+  'https://stormina.students.nomoredomains.monster/*',
+  'http://stormina.students.nomoredomains.monster/*',
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', true);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
