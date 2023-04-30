4526d189cb5d331df14d2014aa7bd2dd6c5b39ea
Максим Сычев <Dethstorm@mail.ru>
2023-04-30 19:35:39 +0300

    добавить метод populate для карточек

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -15,11 +15,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then(({ _id }) => {
-      Card.findById(_id).populate(['owner', 'likes'])
-        .then((card) => res.status(201).send(card))
-        .catch(next);
-    })
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
         next(new BadRequestError('Переданы некорректные данные'));
